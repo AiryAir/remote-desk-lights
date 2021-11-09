5eaf28b8564a1addf7b970dfb1592fc6caaf5dc8
Pratham <69145482+AiryAir@users.noreply.github.com>
2021-11-09 15:50:38 +0530

    Update remote_desklights.ino

--- remote_desklights/remote_desklights.ino
+++ remote_desklights/remote_desklights.ino
@@ -1,17 +1,17 @@
-#include <IRremote.h>                                 //load the IRremote library
+#include <IRremote.h>                               //load the IRremote library
 
-int RECV_PIN = 2;                                     //Set the IRreceiver on pin 2 on the Arduino
+int RECV_PIN = 2;                                  //Set the IRreceiver on pin 2 on the Arduino
 
-IRrecv irrecv(RECV_PIN);                              //Create an instance for irrecv
-decode_results results;                                //Create an instance to store the collected data in
+IRrecv irrecv(RECV_PIN);                           //Create an instance for irrecv
+decode_results results;                            //Create an instance to store the collected data in
 
-int BLED = 9;                                         //Assign the red LED to pin 8
-int GLED = 10;   
-int RLED = 6;                                      //Assign the blue LED to pin 11
+int BLED = 9;                                      //Assign the blue LED to pin 9
+int GLED = 10;                                     //Assign the green LED to pin 10
+int RLED = 6;                                      //Assign the red LED to pin 6
 
 void setup() {
   // put your setup code here, to run once:
-  irrecv.enableIRIn();                          //start the reciever
+  irrecv.enableIRIn();                                //start the reciever
         pinMode(BLED, OUTPUT);                        //set the pin 9 to output
         pinMode(GLED, OUTPUT);                        //set the pin 10 to output
         pinMode(RLED, OUTPUT);                        //set the pin 11 to output
@@ -26,15 +26,15 @@ void loop() {
       //  BLUE
 
       
-      if (results.value == 0xFF30CF){ // button 1: blue low
+      if (results.value == 0xFF30CF){              // button 1: blue low
         analogWrite(BLED, 20);
       }
        irrecv.resume();
-      if (results.value == 0xFF18E7){ // button 2: blue mid
+      if (results.value == 0xFF18E7){             // button 2: blue mid
         analogWrite(BLED, 127);
       }
        irrecv.resume();
-      if (results.value == 0xFF7A85){ // button 3: blue high
+      if (results.value == 0xFF7A85){             // button 3: blue high
         analogWrite(BLED, 230);
       }
        irrecv.resume();
@@ -43,15 +43,15 @@ void loop() {
       //  GREEN
 
       
-      if (results.value == 0xFF10EF){ // button 4: green low
+      if (results.value == 0xFF10EF){             // button 4: green low
         analogWrite(GLED, 20);
       }
       irrecv.resume();
-      if (results.value == 0xFF38C7){ // button 5: green mid
+      if (results.value == 0xFF38C7){            // button 5: green mid
         analogWrite(GLED, 127);
       }
       irrecv.resume();
-      if (results.value == 0xFF5AA5){ // button 6: green high
+      if (results.value == 0xFF5AA5){            // button 6: green high
         analogWrite(GLED, 230);
       }
       irrecv.resume();
@@ -60,23 +60,23 @@ void loop() {
       //    RED
 
 
-      if (results.value == 0xFF42BD){ // button 7: red low
+      if (results.value == 0xFF42BD){          // button 7: red low
         analogWrite(RLED, 20);
       }
       irrecv.resume();
 
-      if (results.value == 0xFF4AB5){ // button 8: red mid
+      if (results.value == 0xFF4AB5){         // button 8: red mid
         analogWrite(RLED, 127);
       }
      irrecv.resume();
 
-     if (results.value == 0xFF52AD){ // button 9: red high
+     if (results.value == 0xFF52AD){          // button 9: red high
         analogWrite(RLED, 230);
       }
      irrecv.resume();
 
      
-      if (results.value == 0xFF6897){ // button 0: turn off all
+      if (results.value == 0xFF6897){         // button 0: turn off all
         analogWrite(BLED, 0);
         analogWrite(GLED, 0);
         analogWrite(RLED, 0);
